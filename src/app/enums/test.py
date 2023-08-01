f56fe95fb118ea34ba09565690530946b475575b
TioSundae <85962841+JoaoVitorBranco@users.noreply.github.com>
2023-08-01 11:37:43 -0300

    nomes fdahoras e commit em pt br Flag: Brazil on Apple iOS 16.4

---
+++ src/app/enums/test.py
@@ -0,0 +1,6 @@
+from enum import Enum
+
+class Enumeracao(Enum):
+    
+    RED = 1
+    GREEN = 2
--- src/app/main.py
+++ src/app/main.py
@@ -1,14 +1,16 @@
 from fastapi import FastAPI
 from mangum import Mangum
 
+from .entities.test_entitie import Entidade
+
 app = FastAPI()
 
 # TODO: Implement my logic here to handle the requests from SimpleFastAPI
 
 @app.get("/")
 def read_root():
+    Entidade.print()
     return {"Hello": "World"}
-
 
 @app.get("/items/{item_id}")
 def read_item(item_id: int):
