1575fa1b65714b1a9849bc2331be3b817d4acf5a
TioSundae <85962841+JoaoVitorBranco@users.noreply.github.com>
2023-08-04 11:32:17 -0300

    refat Environments class to accept .env file

--- src/app/errors/environment_errors.py
+++ src/app/errors/environment_errors.py
@@ -2,5 +2,5 @@ from .base_error import BaseError
 
 
 class EnvironmentNotFound(BaseError):
-    def __init__(self, env: str, message: str):
+    def __init__(self, env: str):
         super().__init__(f'The environment {env} is wrong')
